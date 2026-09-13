7c60f14f8ab429b7b77134cfa5c90feb4739a5b1
anhnghia792003 <155508626+anhnghia792003@users.noreply.github.com>
2026-09-13 09:01:22 +0700

    21AVL_LAB3

---
+++ JavaIOStream/src/JavaIOStream/BinaryFileCopy.java
@@ -0,0 +1,10 @@
+package JavaIOStream;
+
+public class BinaryFileCopy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaIOStream/src/JavaIOStream/Product.java
@@ -0,0 +1,10 @@
+package JavaIOStream;
+
+public class Product {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
